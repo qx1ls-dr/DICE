a610a5249f3811ee378ad4733433c2c6acf28b5b
Леонид Дьяков <amogusr1k@gmail.com>
2026-06-10 21:34:44 +0300

    feat(office): add base scene and game script

---
+++ samples/scripts/office_escape/game.lua
@@ -0,0 +1,19 @@
+local gen = require("scripts.office_escape.gen")
+local grid_w, grid_h = 15, 10
+local tile_size = 64
+
+function init()
+    local grid = gen.generate(grid_h, grid_w)
+    for r = 1, grid_h do
+        for c = 1, grid_w do
+            local type = grid[r][c]
+            local id = "tile_" .. r .. "_" .. c
+            -- Note: In this task we just verify initialization. 
+            -- Entity creation logic will be added in a later task.
+        end
+    end
+    print("Office Escape Initialized")
+end
+
+engine.onKey("W", function() print("Move Up") end)
+init()
